57624c0f4bd4e26341cd923c440c3368c66a91af
nghiach <chnghia@gmail.com>
2021-02-12 21:56:38 +0700

    add fzf with zsh (installed with homebrew

--- setup.sh
+++ setup.sh
@@ -1,5 +1,22 @@
 #!/usr/bin/env bash
 
+COLOR_GRAY="\033[1;38;5;243m"
+COLOR_BLUE="\033[1;34m"
+COLOR_GREEN="\033[1;32m"
+COLOR_RED="\033[1;31m"
+COLOR_PURPLE="\033[1;35m"
+COLOR_YELLOW="\033[1;33m"
+COLOR_NONE="\033[0m"
+
+title() {
+    echo -e "\n${COLOR_PURPLE}$1${COLOR_NONE}"
+    echo -e "${COLOR_GRAY}==============================${COLOR_NONE}\n"
+}
+
+success() {
+    echo -e "${COLOR_GREEN}$1${COLOR_NONE}"
+}
+
 setup_symlinks() {
   title "Creating symlinks"
   cd $HOME
@@ -12,3 +29,8 @@ setup_symlinks() {
   ln -s .dotfiles/zshrc .zshrc
   ln -s .dotfiles/zshrc.local .zshrc.local
 }
+
+setup_symlinks
+
+echo -e
+success "Done."
