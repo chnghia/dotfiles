2b09f9ec70cf1cc01259142350758812af3c8948
nghiach <chnghia@gmail.com>
2021-02-15 13:50:53 +0700

    update config again for m1

--- fzf.zsh
+++ fzf.zsh
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/usr/local/opt/fzf/bin"
+if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}$HOMEBREW_HOME/opt/fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "$HOMEBREW_HOME/opt/fzf/shell/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+source "$HOMEBREW_HOME/opt/fzf/shell/key-bindings.zsh"
--- zprofile
+++ zprofile
@@ -1,3 +1,3 @@
-eval $(/usr/local/Homebrew/bin/brew shellenvhellenv)
+eval $(/opt/homebrew/bin/brew shellenvhellenv)
 export PATH="$HOMEBREW_PREFIX/opt/icu4c/bin:$HOMEBREW_PREFIX/opt/icu4c/sbin:$PATH"
 export PATH="/usr/local/bin:$HOME/bin:$PATH"
